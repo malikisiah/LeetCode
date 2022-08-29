420cf1bcc4b6d598091377c56cc4365725035dd9
Malik Teague <malikisiah214@gmail.com>
2022-08-28 22:35:57 -0700

    1

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -3,38 +3,40 @@
 
 using namespace std;
 
-int search(vector<int>&nums, int target);
+int search(vector<int> &nums, int target);
 
+int main()
+{
 
-int main(){
+    vector<int> nums = {5};
 
-
-vector <int> nums = {5};
-
-
-search(nums,5);
-
+    search(nums, 5);
 }
 
-int search(vector<int>&nums, int target){
+int search(vector<int> &nums, int target)
+{
     int low = 0;
-    int high = nums.size() -1;
+    int high = nums.size() - 1;
 
-        while (low <= high){
-            int mid = low + (high - low) / 2;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target){
-                return mid;
-            }
-
-            if (nums[mid] < target){
-                low = mid+1;
-            }
-
-            else {
-                high = mid-1;
+        if (nums[mid] == target)
+        {
+            return mid;
         }
 
-        return -1;
-}
+        if (nums[mid] < target)
+        {
+            low = mid + 1;
+        }
+
+        else
+        {
+            high = mid - 1;
+        }
+    }
+
+    return -1;
 }
