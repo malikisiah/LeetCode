c77aec1c8606277694602813939dee6dc139f87c
Malik Teague <malikisiah214@gmail.com>
2022-10-19 20:22:20 -0700

    working with stacks !

---
+++ Stack/GenerateParentheses.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<string> generateParentheses(int n)
+{
+    vector<string> result;
+
+    generate(n, 0, 0, "", result);
+
+    return result;
+}
+
+void generate(int n, int open, int close, string str, vector<string> &result)
+{
+    if (open == n && close == n)
+    {
+        result.push_back(str);
+        return;
+    }
+
+    if (open < n)
+    {
+        generate(n, open + 1, close, str + "(", result);
+    }
+
+    if (open > close)
+    {
+        generate(n, open, close + 1, str + ")", result);
+    }
+}
