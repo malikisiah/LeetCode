6a05b627eaccdbcce88d5203af4cd153eb5c307b
Malik Teague <malikisiah214@gmail.com>
2023-04-01 19:28:32 -0700

    one day at a time

--- Backtracking/letterCombinations.cpp
+++ Backtracking/letterCombinations.cpp
@@ -8,6 +8,8 @@ vector<string> letterCombinations(string digits)
     unordered_map<char, string> map = {{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
     string curr = "";
 
+    dfs(digits, 0, map, curr, result);
+
     return result;
 }
 
