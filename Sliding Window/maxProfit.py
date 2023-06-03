3bbd2670fddd5792f480eb952ff9caed1ace7763
Malik Teague <malikisiah214@gmail.com>
2023-06-03 10:09:23 -0700

    sliding these windows

---
+++ Sliding Window/maxProfit.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+
+        for i in range(len(prices)):
+            buy = min(buy, prices[i])
+            profit = max(profit, prices[i] - buy)
+
+        return profit
