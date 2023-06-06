9eebf444be625c2e7d7697822e8c5a15f18e9b33
Malik Teague <malikisiah214@gmail.com>
2023-06-06 07:20:31 -0700

    windows slid

--- Sliding Window/minWindow.py
+++ Sliding Window/minWindow.py
@@ -1,4 +1,36 @@
+from collections import defaultdict
+
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        if t == "":
+            return ""
+
+        countT, window = defaultdict(int), defaultdict(int)
+
+        for i in t:
+            countT[i] += 1
+
+        res, resLen = [-1, -1], float("infinity")
+
         l = 0
-        r = 0
+        have, need = 0, len(countT)
+        for r in range(len(s)):
+            c = s[r]
+            window[c] += 1
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            while have == need:
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                window[s[l]] -= 1
+
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = res
+
+        return s[l : r + 1] if resLen != float("infinity") else ""
