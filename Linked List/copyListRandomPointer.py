e8d9023703bac1ae509e1922842807c6595431b4
Malik Teague <malikisiah214@gmail.com>
2023-06-08 12:09:18 -0700

    linking these lists

---
+++ Linked List/copyListRandomPointer.py
@@ -0,0 +1,32 @@
+from typing import Optional
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: "Node" = None, random: "Node" = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+
+class Solution:
+    def copyRandomList(self, head: "Optional[Node]") -> "Optional[Node]":
+        oldToCopy = {None: None}
+
+        curr = head
+
+        while curr:
+            copy = Node(curr.val)
+
+            oldToCopy[curr] = copy
+
+            curr = curr.next
+
+        curr = head
+
+        while curr:
+            copy = oldToCopy[curr]
+            copy.next = oldToCopy[curr.next]
+            copy.random = oldToCopy[curr.random]
+
+        return oldToCopy[head]
